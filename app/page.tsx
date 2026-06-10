a4c077c84a8bb1316fab02090c8a51ed47fef149
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-10 22:04:13 +0500

    add TopBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@/shared/components/ui/button";
 import Image from "next/image";
-import { Categories, Container, Title } from "@/shared/components/shared";
+import { Categories, Container, Title, SortPopup, TopBar } from "@/shared/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-        <Categories />
       </Container>
+
+      <TopBar />
     </>
   );
 }
