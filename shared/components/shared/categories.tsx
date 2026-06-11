566c580066a6c8e04532f03860984d992a52401f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-11 13:17:47 +0500

    enable global state for categories

--- shared/components/shared/categories.tsx
+++ shared/components/shared/categories.tsx
@@ -1,27 +1,40 @@
+'use client';
+
 import { cn } from "@/shared/lib/utils";
 import React from "react";
+import { useCategoryStore } from "@/store/category";
 
 interface Props {
     className?: string;
 }
 
-const categories: string[] = ['Пиццы', 'Комбо', 'Закуски', 'Коктейли', 'Кофе', 'Напитки', 'Десерты']
-const activeIndex: number = 0
-
+const cats = [
+  { id: 1, name: 'Пиццы' },
+  { id: 2, name: 'Комбо' },
+  { id: 3, name: 'Закуски' },
+  { id: 4, name: 'Коктейли' },
+  { id: 5, name: 'Кофе' },
+  { id: 6, name: 'Напитки' },
+  { id: 7, name: 'Десерты' },
+  { id: 8, name: 'Десерты' },
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+    const categoryActiveId = useCategoryStore((state) => state.activeId)
     return (
         <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
             {
-                categories.map((cat, index) => (
+                cats.map((cat, index) => (
                     <a className={cn(
                         'flex items-center font-bold h-11 rounded-2xl px-5',
-                        activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary',
+                        categoryActiveId === cat.id && 'bg-white shadow-md shadow-gray-200 text-primary',
                         className
-                    )} key={index}>
-                        <button>
-                            { cat }
-                        </button>
+                    )}
+                    href={`/#${cat.name}`}
+                    key={index}>
+                    <button>
+                        { cat.name }
+                    </button>
                     </a>
                 ))
             }
