d5cc9b06691109182e98b6d09afb53c41391f827
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-14 11:54:51 +0500

    add prisma library, create a prisma client and database schema

---
+++ prisma/client.ts
@@ -0,0 +1,14 @@
+import { Node } from './../node_modules/effect/dist/dts/internal/redBlackTree/node.d';
+import { PrismaClient } from "@prisma/client/extension";
+
+const PrismaClientSingleton = () => {
+    return new PrismaClient();
+};
+
+declare global {
+    var prismaGlobal: undefined | ReturnType<typeof PrismaClientSingleton>;
+}
+
+export const prisma = globalThis.prismaGlobal ?? PrismaClientSingleton();
+
+if (process.env.NODE_ENV != "production") globalThis.prismaGlobal = prisma;
