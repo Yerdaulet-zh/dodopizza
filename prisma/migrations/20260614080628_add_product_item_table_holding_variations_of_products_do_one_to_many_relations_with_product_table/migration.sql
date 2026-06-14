ba336ace5181273497ee1d8eca7ef631813fa7e9
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-14 13:06:48 +0500

    add ProductItem table holding variations of products do one to many relations with Product table

---
+++ prisma/migrations/20260614080628_add_product_item_table_holding_variations_of_products_do_one_to_many_relations_with_product_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "ProductItem" (
+    "id" SERIAL NOT NULL,
+    "price" INTEGER NOT NULL,
+    "size" INTEGER,
+    "pizzaType" INTEGER,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductItem" ADD CONSTRAINT "ProductItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
