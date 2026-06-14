5b9dbc89fcf0aa932d35fe4b8e7c71c525e59292
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-14 13:01:00 +0500

    add Ingredient table do many to many relations with table named Product

---
+++ prisma/migrations/20260614080055_add_ingredient_table_do_many_to_many_relations_with_table_named_product/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Ingredient" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ingredient_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Ingredient" ADD CONSTRAINT "Ingredient_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
