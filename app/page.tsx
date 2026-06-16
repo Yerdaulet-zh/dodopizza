349d3f66de058be6f1ddbade646bbcac5a67101d
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-16 12:49:34 +0500

    add product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@/shared/components/ui/button";
 import Image from "next/image";
 import { Filters, Container, Title, TopBar, ProductsGroupList } from "@/shared/components/shared";
+import { prisma } from "@/prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true
+        }
+      }
+    }
+  });
+
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold"/>
       </Container>
 
-      <TopBar />
+      <TopBar categories={categories.filter((category) => category.products.length > 1)}/>
 
       <Container className="mt-10 pb-14">
         <div className="flex gap-18">
@@ -19,126 +31,18 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title="Пиццы"
-                items={[
-                {
-                  id: 1,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 2,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 3,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 4,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 5,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 6,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                }
-                ]}
-                categoryId={1}
-              />
-              <ProductsGroupList
-                title="Комбо"
-                items={[
-                {
-                  id: 1,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 2,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 3,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 4,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 5,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                },
-                {
-                  id: 6,
-                  name: "Маргарита с Песто",
-                  imageUrl: "https://media.dodostatic.net/image/r:584x584/019d485742bd77288f87f77cdab0b347.webp",
-                  price: 599,
-                  items: [{
-                    'price': 599
-                  }]
-                }
-                ]}
-                categoryId={2}
-              />
+              {
+                categories.map((category) => (
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      categoryId={category.id}
+                      title={category.name}
+                      items={category.products}
+                    />
+                  )
+                ))
+              }
             </div>
           </div>
         </div>
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,8 +1,46 @@
+import { prisma } from "@/prisma/client";
+import { Container, GroupVariants, ProductImage, Title } from "@/shared/components/shared";
+import { notFound } from "next/navigation";
+
 export default async function ProductPage({
     params
 }: {
     params: Promise<{ id: string }>
 }) {
     const { id } = await params;
-    return <p>Product ID: {id}</p>;
+    const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+    console.log(product);
+
+    if (!product) {
+        return notFound();
+    }
+
+    return (
+        <Container className="flex flex-col my-10">
+            <div className="flex flex-1">
+                <ProductImage imageUrl={product.imageUrl} size={40} />
+
+                <div className="w-[490px] bg-[#f7f6f5] p-7">
+                    <Title text={product.name} size={'md'} className="font-extrabold mb-1" />
+                    <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est soluta cupiditate, voluptatem minima assumenda dolore amet, explicabo commodi aliquid quis, illo iste! Nihil ipsa illo sit debitis assumenda! Soluta, laborum.</p>
+                    <GroupVariants
+                        items={[
+                            {
+                                name: 'Маленькая',
+                                value: '1'
+                            },
+                            {
+                                name: 'Средняя',
+                                value: '2'
+                            },
+                            {
+                                name: 'Болльшая',
+                                value: '3'
+                            }
+                        ]}
+                    />
+                </div>
+            </div>
+        </Container>
+    );
 }
